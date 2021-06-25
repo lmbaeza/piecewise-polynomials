cd1a3adf32871d7bd4f6d225cb679fb00a650bf7
sorkierkegaard <lmbaeza@unal.edu.co>
2021-06-24 21:59:35 -0500

    Regression Splines fué implementada

---
+++ src/lib.py
@@ -0,0 +1,131 @@
+import math
+import random
+import sys
+
+def cmp(a, b):
+    a = float(a)
+    b = float(b)
+    eps = 1e-6
+    
+    if a + eps < b:
+        return -1
+    else:
+        if b + eps < a:
+            return 1
+    return 0
+
+
+def my_linspace(mn, mx, intervals):
+    distance = mx - mn
+    factor = distance/intervals
+    output = []
+    output.append(float(mn))
+    current = float(mn)
+    while current < float(mx):
+        current += factor
+        output.append(current)
+
+    # return np.linspace(mn, mx, intervals)
+    return output
+
+def my_cos(data):
+    output = []
+    for i in data:
+        delta = random.uniform(-0.5, 0.5)
+        output.append(math.cos(i) + delta)
+    return output
+
+def my_ones(data):
+    output = []
+    for _ in data:
+        output.append(1.0)
+    return output
+
+def my_power(data, n):
+    output = []
+    for val in data:
+        output.append(val**n)
+    return output
+
+def my_transform(data, knot):
+    output = []
+    for val in data:
+        if cmp(val, knot) < 0:
+            output.append(0.0)
+        else:
+            output.append( (val-knot) ** 3 )
+    return output
+
+
+def my_matrix_transpose(m):
+    rez = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
+    return rez
+
+
+def to_col(data):
+    output = []
+    for val in data:
+        output.append([val])
+    return output
+
+def multiply(matrix1, matrix2):
+    row1 = len(matrix1)
+    col1 = len(matrix1[0])
+    
+    row2 = len(matrix2)
+    col2 =  len(matrix2[0])
+    
+    # output = [[0]*col2]*row1
+
+    output = [[0 for _ in range(col2)] for _ in range(row1)]
+    
+    for i in range(row1):
+        for j in range(col2):
+            for k in range(col1):
+                output[i][j] += float(matrix1[i][k] * matrix2[k][j])
+    return output
+
+
+def gauss(a):
+    n = len(a)
+    x = [0.0]*n
+    # Applying Gauss Elimination
+    for i in range(n):
+        if a[i][i] == 0.0:
+            sys.exit('Divide by zero detected!')
+        
+        for j in range(i+1, n):
+            ratio = float(a[j][i]/a[i][i])
+            
+            for k in range(n+1):
+                a[j][k] = float(a[j][k] - ratio * a[i][k])
+    
+    # Back Substitution
+    x[n-1] = float(a[n-1][n]/a[n-1][n-1])
+    
+    for i in range(n-2, -1,-1):
+        x[i] = float(a[i][n])
+        
+        for j in range(i+1,n):
+            x[i] = float(x[i] - a[i][j]*x[j])
+        
+        x[i] = float(x[i]/a[i][i])
+    return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,2 +1,43 @@
-if __name__ == '__main__':
-    print("Hello World")
+import math
+from lib import *
+
+# Axes limits and knots
+x_min, x_knot_1, x_knot_2, x_max = -1, 1.5, 4.5, 7
+
+x_data = my_linspace(x_min, x_max, 50)
+y_data = my_cos(x_data)
+
+y_target = [math.cos(x) for x in x_data]
+
+h1 = my_ones(x_data)
+h2 = x_data.copy()
+h3 = my_power(h2, 2)
+h4 = my_power(h2, 3)
+h5 = my_transform(x_data, x_knot_1)
+h6 = my_transform(x_data, x_knot_2)
+
+H = my_matrix_transpose([h1, h2, h3, h4, h5, h6])
+
+# Ajustar "Basis Expansion" via OLS
+
+HH1 = multiply(my_matrix_transpose(H), H)
+
+HH2 = multiply(my_matrix_transpose(H), to_col(y_data))
+
+for i, val in enumerate(HH2):
+    HH1[i].append(val[0])
+
+beta = gauss(HH1)
+
+y_hat = multiply(H, to_col(beta))
+
+import matplotlib.pyplot as plt
+
+plt.plot(x_data, y_data, 'o')
+plt.plot(x_data, y_target, color='red')
+plt.plot(x_data, y_hat, color='orange')
+plt.title('Cubic Spline')
+
+plt.legend(["data", "Target", "Regression Splines"])
+
+plt.show()
